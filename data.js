bf60b99cf22e9b5c72db32990db4242ddb028bcb
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2023-11-04 20:12:22 +0530

    datatypes

---
+++ data.js
@@ -0,0 +1,9 @@
+let score = 33
+let nam = "Prerit"
+console.log(typeof score);
+console.log(typeof nam);
+
+// Work only with Numbers
+let valueInNumber = Number(nam) // Makes Sure nam is always Number
+console.log(typeof valueInNumber);
+console.log(valueInNumber);     // Gives NaN
--- test.js
+++ test.js
@@ -1,4 +1,14 @@
-const accountId = 12;
-let accountType = 'savings'
+"use strict"; // treat all JavaScript codes as newer version of JavaScript.
 
-console.table([accountId, accountType]);
+const accountId = 12; //number
+let accountType = 'savings' //string
+var accountEmail = "prer@gmail.com"
+accountNo = 1331343434
+let isLoggedIn = false //boolean
+/*
+We can use console.table() to display the data in a table format.
+We dont usually use var for declaring variables. Since it has issues related to scope. (Block/Functional)
+*/
+console.table([accountId, accountType, accountEmail, accountNo]);
+
+
